43ff3043bf82e40ebb522cab0ecdf89ff38ce359
masato_mot <c0a2115063@edu.teu.ac.jp>
2023-06-18 17:35:12 +0900

    2-5-1 単数の弾の発射

--- stg/stg_beta.js
+++ stg/stg_beta.js
@@ -1,17 +1,20 @@
 //==2023/06/13 画面をスクロールさせる==//
 //==2023/06/18 自機を動かす ==//
+//==2023/06/18 単数の弾を発射する ==//
 
 /*起動時の処理*/
 function setup() {
     canvasSize(1200,720);       //キャンバスサイズの設定
     loadImg(0, "image/bg.png"); //画像の読み込み
     loadImg(1, "image/spaceship.png");
+    loadImg(2, "image/missile.png");
     initSShip();
 }
 /*メインループ*/
 function mainloop() {
     drawBG(1);
     moveShip();
+    moveMissile();
 }
 /*背景のスクロール*/
 var bgX = 0; //背景スクロール位置を管理する変数
@@ -34,6 +37,29 @@ function moveShip() {
     if(key[39] > 0 && ssX < 1009)   ssX += 20; //右
     if(key[38] > 0 && ssY > 40)     ssY -= 20; //上
     if(key[40] > 0 && ssY < 680)    ssY += 20; //下
+    if(key[32] == 1) setMissile(ssX+40, ssY, 40, 0); //弾発射
     drawImgC(1,ssX,ssY);//自機の描画
 }
-
+/*弾の管理*/
+var mslX, mslY, mslXp, mslYp;
+var mslF = false; //弾が撃ち出された状態かを管理するフラグ
+/*弾を撃ち出す関数*/
+function setMissile(x,y,xp,yp) {
+    if (mslF == false) {//撃ち出された状態なら
+        //弾の座標と移動量を代入
+        mslX = x;
+        mslY = y;
+        mslXp = xp;
+        mslYp = yp;
+        mslF = true;
+    }
+}
+/*弾を動かす関数*/
+function moveMissile() {
+    if(mslF == true){
+        mslX = mslX + mslXp;
+        mslY = mslY + mslYp;
+        drawImgC(2, mslX, mslY);
+        if(mslX > 1200) mslF = false;
+    }
+}
