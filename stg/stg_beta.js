5613946e426b9dbd4159555e048a432922bfe5a9
masato_mot <c0a2115063@edu.teu.ac.jp>
2023-06-28 10:00:03 +0900

    2-13 シューティングゲームの完成

--- stg/stg_beta.js
+++ stg/stg_beta.js
@@ -10,7 +10,9 @@
 //==2023/06/21 エフェクト(爆発演出)を組み込む==//
 //==2023/06/24 色々な敵機を登場させる==//
 //==2023/06/24 パワーアップアイテムを組み込む==//
-//==2023/06/26 スマートフォン
+//==2023/06/26 スマートフォンに対応させる==//
+//==2023/06/26 シューティングゲームの完成==//
+
 /*起動時の処理*/
 function setup() {
     canvasSize(1200,720);       //キャンバスサイズの設定
@@ -21,32 +23,85 @@ function setup() {
     for(var i=0; i<=4; i++) loadImg(4+i, "image/enemy"+i+".png");
     for(var i=0; i<=2; i++) loadImg(9+i, "image/item"+i+".png");
     loadImg(12, "image/laser.png");
-    initSShip();
+    loadImg(13, "image/title_ss.png");
+    loadSound(0, "sound/bgm.m4a");
     initMissile();
-    initObject();
     initEffect();
 }
 /*メインループ*/
 var tmr = 0; //ゲーム内タイマー
+var idx = 0; //ゲームシーンを区別する為の変数
+var score = 0;
+var stage = 0;
 function mainloop() {
     tmr++;
     drawBG(1);
-    moveShip();
-    moveMissile();
-    moveObject();
-    drawEffect();
-    /*エネルギー描画*/
-    for(i=0; i<10; i++) fRect(20+i*30, 660, 20, 40, "#c00000"); //残機0のエネルギー
-    for(i=0; i<energy; i++) fRect(20+i*30, 660, 20, 40, colorRGB(160-16*i, 240-12*i, 24*i));//エネルギーの残量を描く
-    setEnemy();
-    setItem();
+    switch(idx){
+        /*タイトル画面*/
+        case 0:
+        drawImg(13,200,200);
+        if(tmr%40 < 20) fText("Press [SPC] or Click to start.", 600, 540, 40,"cyan"); //点滅動作
+        if(key[32] > 0 || tapC > 0){//スペースキーまたはクリックしたら
+            initSShip(); 
+            initObject();
+            score = 0;
+            stage = 1;
+            idx = 0;
+            tmr = 0;
+            playBgm(0);
+        }
+        break;
+        /*ゲーム中*/
+        case 1:
+        setEnemy();
+        setItem();
+        moveShip();
+        moveMissile();
+        moveObject();
+        drawEffect();
+        /*エネルギー描画*/
+        for(i=0; i<10; i++) fRect(20+i*30, 660, 20, 40, "#c00000"); //残機0のエネルギー
+        for(i=0; i<energy; i++) fRect(20+i*30, 660, 20, 40, colorRGB(160-16*i, 240-12*i, 24*i));//エネルギーの残量を描く
+        if(tmr < 30*4) fText("STAGE "+stage, 600, 300, 50, "cyan");//ステージ紹介
+        if(30*114 < tmr && tmr < 30*118) fText("STAGE CLEAR", 600, 300, 50, "cyan");//ゲームクリア表示
+        /*次のステージへ*/
+        if(tmr == 30*120){
+            stage++;
+            tmr = 0;
+        }
+        break;
+        /*ゲームオーバー*/
+        case 2:
+        if(tmr < 30*2 && tmr % 5 == 1) setEffect(ssX+rnd(120)-60, ssY+rnd(80)-40, 9); //ゲームオーバー時のエフェクト
+        moveMissile();
+        moveObject();
+        drawEffect();
+        fText("GAME OVER", 600, 300, 50, "red");
+        if(tmr > 30*5) idx = 0;//自動的にゲームタイトル移動
+        break;
+    }
 }
+
 /*背景のスクロール*/
 var bgX = 0; //背景スクロール位置を管理する変数
 function drawBG(spd) {//背景をスクロール位置を管理する
     bgX = (bgX + spd) % 1200;
     drawImg(0, -bgX, 0);
     drawImg(0, 1200-bgX, 0);//左右に2枚の画像を並べて描く
+    //=======================立体的な地面を描くコード=======================//
+    var hy = 580; //地面の地平線のY座標
+    var ofsx = bgX % 40;//縦のラインを移動させるオフセット値
+    lineW(2);
+    for(var i=1; i<=30; i++){//縦のライン
+        var tx = i * 40 - ofsx;//線の奥側のX座標
+        var bx = i * 240 - ofsx * 6 - 3000;//線の手前側のX座標
+        line(tx,hy,bx,720,"silver");
+    }
+    for(var i=1; i<12; i++){//横のライン
+        lineW(1+int(i/3));
+        line(0, hy, 1200, hy, "gray");
+        hy = hy + i* 2;
+    }
 }
 /*自機の管理*/
 var ssX = 0;
@@ -257,10 +312,33 @@ function drawEffect() {
 }
 /*4種類の敵を出現させる関数*/
 function setEnemy() {
-    if(tmr%60 == 0) setObject(1, 5, 1300, 60+rnd(600), -16, 0, 1);//敵機1
-    if(tmr%60 ==10) setObject(1, 6, 1300, 60+rnd(600), -12, 8, 3);//敵機2
-    if(tmr%60 ==20) setObject(1, 7, 1300, 360+rnd(300), -48, -10, 5);//敵機3
-    if(tmr%60 ==30) setObject(1, 8, 1300, rnd(720-192), -6, 0, 0);//障害物
+    var sec = int(tmr/30);
+    if(4 <= sec && sec < 10){
+        if(tmr%20 == 0) setObject(1, 5, 1300, 60+rnd(600), -16, 0, 1*stage);//敵機1    
+    }
+    if(14 <= sec && sec < 20){
+        if(tmr%20 == 0) setObject(1, 6, 1300, 60+rnd(600), -12, 8, 3*stage);//敵機2    
+    }
+    if(24 <= sec && sec < 30){
+        if(tmr%20 == 0) setObject(1, 7, 1300, 360+rnd(300), -48, -10, 5*stage);//敵機3    
+    }
+    if(34 <= sec && sec < 50){
+        if(tmr%60 == 0) setObject(1, 8, 1300, rnd(720-192), -6, 0, 0);//障害物        
+    }
+    if(54 <= sec && sec < 70){
+        if(tmr%20 == 0){
+            setObject(1, 5, 1300,  60+rnd(600), -16,  4, 1*stage);//敵機1
+            setObject(1, 5, 1300, 360+rnd(600), -16, -4, 1*stage);//敵機1
+        }
+    }
+    if(74 <= sec && sec < 90){
+        if(tmr%20 == 0) setObject(1, 6, 1300, 60+rnd(600), -12, 8, 3*stage);//敵機2
+        if(tmr&45 == 0) setObject(1, 8, 1300, rnd(720-192), -8, 0, 0);//障害物     
+    }
+    if(94 <= sec && sec < 110){
+        if(tmr%10 == 0) setObject(1, 5, 1300, 360, -24 , rnd(11)-5, 1*stage);//敵機1
+        if(tmr%20 == 0) setObject(1, 7, 1300, rnd(300), -56, 4+rnd(12), 5*stage);//敵機3 
+    }
 }
 /*複数の弾を一度に放つ関数*/
 function setWeapon() {
